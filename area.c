0b9a5b1b6dac4b910bcd509cc8f4bbd0c43716d7
le-wis-e <muthugumilewis68@gmail.com>
2024-09-12 20:27:13 +0300

    C programs

---
+++ area.c
@@ -0,0 +1,11 @@
+//A program to find the rea of a rectangle
+#include <stdio.h> //scanf(), printf()
+int main() {
+    //dec and initialization
+    int Length = 15;
+    int Width = 10;
+    int area = Length * Width;
+    printf("The area of the rectangle is: %d\n",area);
+
+    return 0;
+}
---
+++ sum of two numbers.c
@@ -0,0 +1,10 @@
+//program to find the sum of two numbers 
+#include <stdio.h>
+int main() {
+    int a = 1300,b = 1600,c = 1000,sum; // dec and initialization
+    sum = a + b + c;
+    printf("The sum of the rectangle  is a b and c %d", sum);
+    
+return 0;
+
+}
